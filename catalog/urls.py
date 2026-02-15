18711f1d345d71a7e57d17668f42ea010bbe6648
Evgenii_Lobachev <lobachev9614@gmail.com>
2026-02-15 21:27:55 +0300

    add add_product.html, product_added.html
    added a controller "add_product"

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contacts/", views.contacts, name="contacts"),
     path("product_item/<int:product_id>/", views.product_item, name="product_item"),
+    path("add_product/", views.add_product, name="add_product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,16 @@
 from typing import Any
 
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
-from catalog.models import Product, Contacts
+from catalog.models import Product, Contacts, Category
 
 
 def index(request: HttpRequest) -> Any:
     """Контроллер принимающий GET запрос и возвращающий представление главной страницы проекта."""
 
     # получение списка продуктов из БД
-    context = {"products": Product.objects.all()}
+    context = {"products": get_list_or_404(Product)}
     # генерация HTML-кода при GET-запросе (главной страницы каталога)
     return render(request, "catalog/index.html", context)
 
@@ -23,7 +23,7 @@ def contacts(request: HttpRequest) -> Any:
         # генерация HTML-кода при POST-запросе(при заполнении и отправке формы)
         return render(request, "catalog/message.html", {"name": name})
     # вывод данных из БД таблицы 'contacts' для отображения на странице 'Контакты'
-    context = {"contact": Contacts.objects.get()}
+    context = {"contact": get_object_or_404(Contacts)}
     # генерация HTML-кода при GET-запросе (страницы Контактов)
     return render(request, "catalog/contacts.html", context)
 
@@ -36,6 +36,23 @@ def product_item(request: HttpRequest, product_id: int) -> Any:
         # генерация HTML-кода при POST-запросе(при заполнении и отправке формы)
         return render(request, "catalog/message.html", {"name": name})
     # получение объекта-продукт из БД по уникальному идентификатору
-    context = {"product": Product.objects.get(id=product_id)}
-    # генерация HTML-кода при GET-запросе (главной страницы каталога)
+    context = {"product": get_object_or_404(Product, id=product_id)}
+    # генерация HTML-кода при GET-запросе (страницы продукта)
     return render(request, "catalog/product_item.html", context)
+
+
+def add_product(request: HttpRequest) -> Any:
+    """Контроллер принимающий GET запрос и возвращающий представление страницы для добавления продукта."""
+
+    if request.method == "POST":
+        # создание объекта-продукт и добавление его в БД
+        category_id = int(request.POST.get("category"))
+        Product.objects.create(name=request.POST.get("name"), description=request.POST.get("description"),
+                               category=Category.objects.get(id=category_id), price=request.POST.get("price"),
+                               image=request.POST.get("image"))
+        # генерация HTML-кода при POST-запросе(при заполнении данных о продукте и отправке формы)
+        return render(request, "catalog/product_added.html")
+    # получение списка продуктов из БД
+    context = {"categories": get_list_or_404(Category)}
+    # генерация HTML-кода при GET-запросе (главной страницы каталога)
+    return render(request, "catalog/add_product.html", context)
