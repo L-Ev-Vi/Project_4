e432c69f7cf8f9604622e788a2d050333ada9273
Evgenii_Lobachev <lobachev9614@gmail.com>
2026-03-02 01:11:19 +0300

    update README.md

--- catalog/models.py
+++ catalog/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
 
     name: str = models.CharField(max_length=150, verbose_name="Наименование")
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
-    image = models.ImageField(upload_to="images/", default="images/default.jpg", null=True, blank=True, verbose_name="Изображения")
+    image = models.ImageField(upload_to="images/", default="images/default.jpg", blank=True, verbose_name="Изображения")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product", verbose_name="категория")
     price = models.FloatField(verbose_name="Цена")
     publication = models.BooleanField(default=True, verbose_name="Признак публикации")
